0e9d8b1753860aa5d694a3a881d31929d3158a18
sushmeet sunger <ssunger@shutterstock.com>
2020-07-25 09:21:23 -0400

    work for lessen 1

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -12,4 +13,44 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const PORT = 3000
+const HOSTNAME = 'http://localhost'
+
+// middleware function
+// use it to modify stuff and then move on to the next middleware or controller.
+const log = (req, res, next) => {
+    console.log('Middleware Log Run ')
+    next()
+}
+
+
+// register the router
+app.use('/api', router)
+
+// router functions
+router.get('/me', (req, res) => {
+    res.json({
+        name: 'sushi'
+    })
+})
+
+//controller function
+// middleware and controller are basically the same thing it just thier intention is different.
+app.get('/home', log, (req, res) => {
+    res.status(200).json({
+        name: 'sushi'
+    })
+})
+
+app.post('/home', (req, res) => {
+    console.log(req.body);
+    res.json({message: 'ok'})
+})
+
+
+export const start = () => {
+    const server = app.listen(PORT, () => {
+        console.log(`server has started on ${PORT}`)
+    })
+return server
+}
