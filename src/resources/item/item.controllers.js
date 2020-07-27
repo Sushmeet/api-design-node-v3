b5a28b95c04615f912fd6f2a9483dbd98b26e4a4
sushmeet sunger <ssunger@shutterstock.com>
2020-07-27 00:16:59 -0400

    update controller logic

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,4 +1,49 @@
 import { crudControllers } from '../../utils/crud'
+// import mongoose from 'mongoose'
 import { Item } from './item.model'
+// import { connect } from '../../utils/db'
 
 export default crudControllers(Item)
+
+// const run = async () => {
+//   await connect('mongodb://localhost:27017/api-test')
+//   const item = await Item.create({
+//     name: 'Sushi Item',
+//     createdBy: mongoose.Types.ObjectId(),
+//     list: mongoose.Types.ObjectId()
+//   })
+//   console.log(Item)
+//   return item
+// }
+
+// run()
+// .then(res => console.log('response man-------', res))
+// .catch(err => console.log(err))
+
+
+
+// const findAllItems = async () => {
+//   await connect('mongodb://localhost:27017/api-test')
+
+//   const itemsFound = await Item.find()
+//   // console.log('---------------------', itemsFound)
+
+//   const itembyId5f1da68a4c7c1a72b9d9512a = await Item.findById(
+//     '5f1da68a4c7c1a72b9d9512a'
+//   )
+// //   console.log('--------itembyId----------', itembyId5f1da68a4c7c1a72b9d9512a)
+
+//   const itemUpdateById5f1da68a4c7c1a72b9d9512a = await Item.findOneAndUpdate(
+//     {
+//       _id: '5f1da68a4c7c1a72b9d9512a'
+//     },
+//     { name: 'Jooombaaas Items222' 
+//     },
+//     // {new: true}
+//   ).exec()
+
+//   console.log('update------', itemUpdateById5f1da68a4c7c1a72b9d9512a)
+
+// }
+
+// findAllItems()
--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,89 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const userId = req.user['_id'];
+  const listId = req.params.id
 
-export const getMany = model => async (req, res) => {}
+  const doc = await model.findOne({
+    createdBy: userId,
+    _id: listId
+  }).exec()
 
-export const createOne = model => async (req, res) => {}
+  if(!doc) {
+    return res.status(400).end()
+  }
+  
+  res.status(200).json({
+    data: doc
+  })
+}
 
-export const updateOne = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const userId = req.user._id
 
-export const removeOne = model => async (req, res) => {}
+  const doc = await model.find({
+    createdBy: userId
+  })
+
+  res.status(200).json({
+    data: doc
+  })
+
+}
+
+export const createOne = model => async (req, res) => {
+  const name = req.body.name
+  const userId = req.user._id
+
+  const doc = await model.create({
+    createdBy: userId,
+    name
+  })
+  res.status(201).json({ data: doc})
+
+}
+
+export const updateOne = model => async (req, res) => {
+  const userId = req.user._id
+  const listId = req.params.id
+  const name = req.body
+
+  const doc = await model.findOneAndUpdate(
+    {
+      _id: listId,
+      createdBy: userId
+    },
+     name,
+    {
+      new: true
+    }
+  )
+
+  if(!doc) {
+    return res.status(400).end()
+  }
+
+  res.status(200).json({
+    data: doc
+  })
+
+}
+
+export const removeOne = model => async (req, res) => {
+  const userId = req.user._id
+  const listId = req.params.id
+
+  const doc = await model.findOneAndRemove({
+    _id: listId,
+    createdBy: userId
+  })
+  
+  if(!doc) {
+    return res.status(400).end()
+  }
+
+  res.status(200).json({
+    data: doc
+  })
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
